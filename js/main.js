5bae9f2e32dd3b7cf8125eabb3f691425ba85f1d
solmii <rkdthfal4759@naver.com>
2020-06-04 23:29:24 +0900

    JS : add comment event

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 'use strict';
 
-let commentBtn = document.getElementsByClassName('comment_btn')[0];
-let userComment = document.getElementsByTagName('textarea')[0];
-let commentForm = document.getElementsByClassName('comment_form')[0];
+const commentBtn = document.getElementsByClassName('comment_btn')[0];
+const inputValue = document.getElementsByTagName('textarea')[0];
+const commentUpload = document.getElementsByClassName('comment_form')[0];
+const comments = document.getElementsByClassName('comments')[0];
 
 // 댓글 입력시 게시 버튼 색 변화
-userComment.addEventListener('keyup', function () {
-  commentBtn.style.opacity = userComment.value ? 1 : 0.3;
-  commentBtn.style.cursor = userComment.value ? 'pointer' : 'Default';
-  commentBtn.disabled = userComment.value ? true : false;
+inputValue.addEventListener('keyup', function () {
+  commentBtn.style.opacity = inputValue.value ? 1 : 0.3;
+  commentBtn.style.cursor = inputValue.value ? 'pointer' : 'Default';
+  commentBtn.disabled = inputValue.value ? false : true;
 });
 
-// function uploadComment() {
-//   if (userComment.value.length > 1) {
-//   }
-// }
+// 댓글 입력시 새 댓글창 추가
 
-// submicommentBtntBtn.addEventListener('click', uploadComment);
+commentBtn.addEventListener('click', function () {
+  let commentUpload = document.createElement('div');
+  let userId = document.createElement('a');
+  let commentValue = document.createElement('span');
+  let delBtn = document.createElement('button');
+
+  if (inputValue.value) {
+    commentUpload.className = 'comment_form';
+    userId.className = 'comment_id';
+    commentValue.className = 'comment_style';
+    delBtn.className = 'js-delBtn';
+
+    userId.innerHTML = 'Wecoder';
+    delBtn.innerHTML = '삭제';
+    commentValue.innerHTML = inputValue.value;
+
+    comments.appendChild(commentUpload);
+    commentUpload.appendChild(userId);
+    commentUpload.appendChild(delBtn);
+    commentUpload.appendChild(commentValue);
+  }
+});
